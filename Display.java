730ec1c1cea5e105417a4cecd0c97aea5c47b4f0
kellcj2 <kellcj2@comcast.net>
2020-11-01 19:01:55 -0500

    combined addEvent and modifyEvent to single function

--- Display.java
+++ Display.java
@@ -34,22 +34,23 @@ public class Display extends Application {
 	private Scene showEvents(Stage stage) {
 		Button addButton = new Button("Add Event");
 
+		// handler for "Add Event" button
 		EventHandler<ActionEvent> addEvent = new EventHandler<ActionEvent>() { 
 				@Override 
 				public void handle(ActionEvent e) {
-					Scene addScene = addEvent(stage);
+					Scene addScene = addModifyEvent(stage, -1);
 					stage.setScene(addScene);
 					stage.show();
 				} 
 			};
-
+		
 		EventHandler<ActionEvent> modifyEvent = new EventHandler<ActionEvent>() {
 				@Override 
 				public void handle(ActionEvent e) {
 					Object o = e.getSource();
 					if(o instanceof Button) {
 						int id = ((int)((Button) o).getUserData());
-						Scene modScene = modifyEvent(stage, id);
+						Scene modScene = addModifyEvent(stage, id);
 						stage.setScene(modScene);
 						stage.show();
 					}
@@ -65,8 +66,8 @@ public class Display extends Application {
 		// display all events in calendar
 		ArrayList<Event> allEvents = cal.getAllEvents();
 		for(int i=0; i < cal.getNumEvents(); i++) {
+			// get data from the current event
 			Event ev = allEvents.get(i);
-			
 			DateTimeFormatter f = DateTimeFormatter.ofPattern("dd LLLL, yyyy  ");
 			Label lName = new Label(ev.getName());
 			lName.setFont(new Font("Arial", 18));
@@ -75,132 +76,75 @@ public class Display extends Application {
 			Button modify = new Button("Modify");
 			modify.setUserData(ev.getId()); // keep track of event id
 			modify.setOnAction(modifyEvent);
-			root.addRow(i+1, lName, lDate, lTime, modify);
+			root.addRow(i+1, lName, lDate, lTime, modify); // add this event to root
 		}
 		
 		return new Scene(root, 600, 600);		
 	}
 
-	private Scene addEvent(Stage stage) {
-		System.out.println("add event");
+	/* Name: addModifyEvent
+	   Desc: creates scene for adding a new event or modifying
+	         an existing one
+	   Param: Stage stage - main stage for adding Scenes
+	          int id - (If == -1), add a new Event to calendar.
+			           Otherwise, lookup Event by id and modify it.
+	   Return: a Scene for adding / modifying events			          
+	 */
+	private Scene addModifyEvent(Stage stage, int id) {
+		final Event modEvent = cal.getAllEventsHashMap().get(id);
+		Button submit, delete;
+		TextField name, desc;
+		DatePicker datePicker;
+		// data for modifying event
+		String eventName = "", eventDesc = "";
+		LocalDate eventDate = null;
+		LocalTime eventTime = null;
+p		
 		GridPane gp = new GridPane();
 		gp.setAlignment(Pos.CENTER);
 		gp.setPrefWidth(400);
-		Label error = new Label();
+		Label error = new Label(); // for invalid inputs
 		error.setTextFill(Color.RED);
-		
-		Button submit = new Button("Confirm");
-		TextField name = new TextField();
-		TextField desc = new TextField();
-		DatePicker datePicker = new DatePicker(); // date picker
+
+		if(id == -1) { // add a new event
+			submit = new Button("Add Event");
+		} else { // modify existing event
+			submit = new Button("Confirm Modification");
+			eventName = modEvent.getName();
+			eventDesc = modEvent.getDescription();			
+			eventDate = modEvent.getDate();
+			eventTime = modEvent.getTime();
+		}
+
+		name = new TextField(eventName);
+		desc = new TextField(eventDesc);
+		datePicker = new DatePicker(eventDate); // date picker
 		// add AM/PM buttons
 		ComboBox<String> hours = new ComboBox<String>();
 		ObservableList<String> list = hours.getItems();
-		for(int i = 1; i <= 12; i++) // add options to hours
+		for(int i = 1; i <= 12; i++) // add 1-12 to hours
 			list.add(String.valueOf(i));
-		
+
 		ComboBox<String> minutes = new ComboBox<String>();
 		list = minutes.getItems();
-		for(int i = 0; i < 60; i += 15) // add to minutes
+		for(int i = 0; i < 60; i += 15) // add 0,15,30,45 to minutes
 			list.add(String.valueOf(i));
 		
 		ComboBox<String> ampm = new ComboBox<String>();
-		ampm.getItems().addAll("AM", "PM");
+		ampm.getItems().addAll("AM", "PM"); // AM PM option
 		
-		// add the event, go back to Show Events screen
-		EventHandler<ActionEvent> confirm = new EventHandler<ActionEvent>() { 
-				@Override 
-				public void handle(ActionEvent e) {
-					String eName = name.getText();
-					String eDesc = desc.getText();
-					LocalDate eDate = datePicker.getValue();
-					String eAMPM = ampm.getValue();
-					int eHour = -1, eMin = -1;
-					if(hours.getValue() != null) // convert to int
-						eHour = Integer.parseInt(hours.getValue());
-					if(minutes.getValue() != null)
-						eMin = Integer.parseInt(minutes.getValue());
-
-					if(eName.isEmpty()) // check for blank fields
-						error.setText("Name is empty");
-					else if(eDesc.isEmpty())
-						error.setText("Description is empty");
-					else if(eDate == null)
-						error.setText("Date is empty");
-					else if(eHour == -1)
-						error.setText("Hour is empty");
-					else if(eMin == -1)
-						error.setText("Minute is empty");
-					else if(eAMPM == null)
-						error.setText("AM/PM is empty");
-					else { // all good
-						// add event to calendar
-						if(eAMPM == "PM") // convert to 24 hour clock
-							eHour += 12;
-						LocalTime eTime = LocalTime.of(eHour, eMin, 0);
-						Event event = new Event(eName, eDesc, eDate, eTime);
-						cal.addEvent(event);
-						// go back to "Show Events"
-						Scene sEvents = showEvents(stage);
-						stage.setScene(sEvents);
-						stage.show();
-					}
-				} 
-			};
-
-		submit.setOnAction(confirm);
-		
-		gp.addRow(0, new Label("Event Name:"), name);
-		gp.addRow(1, new Label("Description:"), desc);
-		gp.addRow(2, new Label("Date:"), datePicker);
-		gp.addRow(3, new Label("Hour:"), hours);
-		gp.addRow(4, new Label("Minute:"), minutes);
-		gp.addRow(5, new Label("AM/PM:"), ampm);
-		gp.addRow(6, submit, error);
-
-		return new Scene(gp, 600, 600);
-	}
-
-	// same code as addEvent but with event values filled in
-	// dont know of a better way to do it, default parameters dont exist in java
-	private Scene modifyEvent(Stage stage, int id) {
-		System.out.println("modify event");
-		Event modEvent = cal.getAllEventsHashMap().get(id);
-		GridPane gp = new GridPane();
-		gp.setAlignment(Pos.CENTER);
-		gp.setPrefWidth(400);
-		Label error = new Label();
-		error.setTextFill(Color.RED);
-		
-		Button submit = new Button("Confirm Modification");
-		Button delete = new Button("Delete Event");
-		TextField name = new TextField(modEvent.getName());
-		TextField desc = new TextField(modEvent.getDescription());
-		DatePicker datePicker = new DatePicker(modEvent.getDate()); // date picker
-		// add AM/PM buttons
-		ComboBox<String> hours = new ComboBox<String>();
-		ObservableList<String> list = hours.getItems();
-		for(int i = 1; i <= 12; i++) // add options to hours
-			list.add(String.valueOf(i));
-
-		
-		ComboBox<String> minutes = new ComboBox<String>();
-		list = minutes.getItems();
-		for(int i = 0; i < 60; i += 15) // add to minutes
-			list.add(String.valueOf(i));
-		minutes.setValue(String.valueOf(modEvent.getTime().getMinute()));
-		
-		ComboBox<String> ampm = new ComboBox<String>();
-		ampm.getItems().addAll("AM", "PM");
-		if(modEvent.getTime().getHour() > 12) {
-			hours.setValue(String.valueOf(modEvent.getTime().getHour() - 12));
-			ampm.setValue("PM");
-		} else {
-			hours.setValue(String.valueOf(modEvent.getTime().getHour()));
-			ampm.setValue("AM");
+		if(eventTime != null) { // load time if modifying event
+			minutes.setValue(String.valueOf(eventTime.getMinute()));
+			if(modEvent.getTime().getHour() > 12) {
+				hours.setValue(String.valueOf(eventTime.getHour() - 12));
+				ampm.setValue("PM");
+			} else {
+				hours.setValue(String.valueOf(eventTime.getHour()));
+				ampm.setValue("AM");
+			}
 		}
 		
-		// modify the event, go back to Show Events screen
+		// Confirm button handler, verify non-empty, go to Show Events Scene
 		EventHandler<ActionEvent> confirm = new EventHandler<ActionEvent>() { 
 				@Override 
 				public void handle(ActionEvent e) {
@@ -227,13 +171,14 @@ public class Display extends Application {
 					else if(eAMPM == null)
 						error.setText("AM/PM is empty");
 					else { // all good
-						// modify event
 						if(eAMPM == "PM") // convert to 24 hour clock
 							eHour += 12;
 						LocalTime eTime = LocalTime.of(eHour, eMin, 0);
 						Event event = new Event(eName, eDesc, eDate, eTime);
-						cal.removeEvent(id); // remove old event
-						cal.addEvent(event); // add modified event
+						if(modEvent != null) // modifying event, remove old event
+							cal.removeEvent(id);
+						cal.addEvent(event); // add event
+						
 						// go back to "Show Events"
 						Scene sEvents = showEvents(stage);
 						stage.setScene(sEvents);
@@ -251,7 +196,8 @@ public class Display extends Application {
 					stage.show();
 				}
 			};
-				
+		
+		delete = new Button("Delete Event");				
 		submit.setOnAction(confirm);
 		delete.setOnAction(deleteAction);
 		
@@ -262,14 +208,15 @@ public class Display extends Application {
 		gp.addRow(4, new Label("Minute:"), minutes);
 		gp.addRow(5, new Label("AM/PM:"), ampm);
 		gp.addRow(6, submit, error);
-		gp.addRow(7, delete);
+		if(id != -1)
+			gp.addRow(7, delete);
 
 		return new Scene(gp, 600, 600);
 	}
 
 	@Override
 	public void start(Stage stage) {
-        Scene baseScene = showEvents(stage);
+        Scene baseScene = showEvents(stage); // load the "Show Events" Scene
 		stage.setScene(baseScene);
 		stage.setTitle("CSC 520 Calendar Application");
         stage.show();
